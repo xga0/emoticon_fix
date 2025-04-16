c1231ae5adae3aa7aed92c1f9b3ddc4ff08b7095
Xiangyu Gao <seangaoxy@gmail.com>
2025-04-16 12:58:37 -0700

    fix: expose new functions in __init__.py

--- emoticon_fix/__init__.py
+++ emoticon_fix/__init__.py
@@ -1,3 +1,3 @@
-from .emoticon_fix import emoticon_fix
+from .emoticon_fix import emoticon_fix, remove_emoticons, replace_emoticons
 
-__all__ = ['emoticon_fix']
+__all__ = ['emoticon_fix', 'remove_emoticons', 'replace_emoticons']
